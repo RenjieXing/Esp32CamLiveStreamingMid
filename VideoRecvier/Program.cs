af0b7d4da472056d5d2bacc28a2ec201e7e7277e
Renjie Xing <xingrenjie1hao@gmail.com>
2025-02-10 03:55:05 +0800

    update

--- VideoRecvier/Program.cs
+++ VideoRecvier/Program.cs
@@ -1,30 +1,31 @@
-﻿using System;
+﻿using OpenCvSharp;
+using OpenCvSharp.Flann;
+using System;
+using System.Collections;
 using System.Collections.Concurrent;
 using System.Dynamic;
-using System.IO;
 using System.Net;
 using System.Net.Sockets;
-using System.Text;
-using System.Threading;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
-namespace VideoRecvier
+namespace VideoReceiver
 {
-    public static class rocerd
+    public static class Recorder
     {
-        public static bool init()
+        public static bool Init()
         {
-            var exeDirectory = AppDomain.CurrentDomain.BaseDirectory; // 获取可执行文件目录
+            var exeDirectory = AppDomain.CurrentDomain.BaseDirectory;
             var appSettingsPath = Path.Combine(exeDirectory, "appsetting.json");
             if (File.Exists(appSettingsPath))
             {
-                var JsonFile = File.ReadAllText(appSettingsPath);
-                var JsonObj = System.Text.Json.JsonSerializer.Deserialize<ExpandoObject>(JsonFile);
-                var dictionary = JsonObj as IDictionary<string, object>;
+                var jsonFile = File.ReadAllText(appSettingsPath);
+                var jsonObj = System.Text.Json.JsonSerializer.Deserialize<ExpandoObject>(jsonFile);
+                var dictionary = jsonObj as IDictionary<string, object>;
                 FFmpegPath = dictionary?["FFmpegPath"]?.ToString() ?? "";
                 Mp4FileRoot = dictionary?["Mp4FileRoot"]?.ToString() ?? "";
                 M3U8FileRoot = dictionary?["M3U8FileRoot"]?.ToString() ?? "";
                 MaxSegments = int.Parse(dictionary?["MaxSegments"]?.ToString() ?? "5");
-                OUTDATEtIME = int.Parse(dictionary?["OUTDATEtIME"]?.ToString() ?? "20");
+                OutdateTime = int.Parse(dictionary?["OUTDATEtIME"]?.ToString() ?? "20");
                 MediaType = dictionary?["ListenMediaType"]?.ToString() ?? "mp4";
                 return true;
             }
@@ -32,45 +33,100 @@ namespace VideoRecvier
         }
 
         public static string FFmpegPath = "";
-
         public static string Mp4FileRoot = "";
-
         public static string M3U8FileRoot = "";
-
         public static int MaxSegments = 5;
-
-        public static int OUTDATEtIME = 20;
-
+        public static int OutdateTime = 20;
         public static string MediaType = "mp4";
     }
 
     class Program
     {
-        static ConcurrentQueue<string> fileQueue = new ConcurrentQueue<string>();
-        static int maxQueueSize = 10; // 最大队列大小
+        static int frameCount = 0;
         static ManualResetEvent allDone = new ManualResetEvent(false);
-
+        static ConcurrentQueue<List<Byte>> imageQueue = new ();
+        static List<Byte> docker=new();
+        static int fileLenth = int.MinValue;
         static void Main(string[] args)
         {
-            if (!rocerd.init())
+            if (!Recorder.Init())
             {
                 Console.WriteLine("配置文件不存在");
                 Console.ReadLine();
                 return;
             }
+            Task.Run(() => MoveToMP4());
             StartListening();
         }
 
+        public static async void MoveToMP4()
+        {
+
+
+            // 图像文件夹路径
+            string imageFolder = Recorder.Mp4FileRoot;
+            int duration = 5;
+            // 输出视频文件路径
+            string outputVideo = Recorder.Mp4FileRoot;
+            int height = 800;
+            int width = 600;
+            int Fps = 10;
+
+            while (true)
+            {
+                await Task.Delay(duration * 1000);
+                var frameCount = imageQueue.Count;
+                if (frameCount == 0)
+                {
+                    continue;
+                }
+                List<Byte[]> bytes = new();
+                // 创建视频写入器
+                VideoWriter videoWriter = new VideoWriter(Path.Combine(outputVideo,$"{frameCount}.mp4"), VideoWriter.FourCC('m', 'p', '4', 'v'), Fps, new OpenCvSharp.Size(width, height));
+                if(frameCount !=Fps)
+                {
+                    for (var i = 0; i < frameCount; i++)
+                    {
+                        if (imageQueue.TryDequeue(out var buffer))
+                        {
+                            bytes.Add(buffer.ToArray());
+                            FileStream fileStream = new FileStream($"{i}.jpeg", FileMode.Append, FileAccess.Write);
+                            fileStream.Write(buffer.ToArray(), 0, buffer.Count-1);
+                            fileStream.Close();
+                        }
+                    }
+                }
+                var RATE = ((double)frameCount) / ((double)Fps);
+                for (var i = 0; i < Fps* duration; i++)
+                {
+                 // 写入每张图像到视频中
+                  Mat image = Cv2.ImDecode(bytes[i * RATE>=bytes.Count? bytes.Count-1: (int)(i * RATE)], ImreadModes.Color);
+                 if (image.Empty())
+                  {
+                        Console.WriteLine($"Failed to decode image ");
+                        continue;
+                  }
+                    videoWriter.Write(image);
+                }
+                // 释放视频写入器
+                videoWriter.Release();
+            }
+
+            
+        }
+
+        
         public static void StartListening()
         {
-            IPAddress ipAddress = IPAddress.Parse("127.0.0.1");
+            IPAddress ipAddress = Dns.GetHostEntry(Dns.GetHostName())
+                                     .AddressList.First(ip => ip.AddressFamily == AddressFamily.InterNetwork);
             IPEndPoint localEndPoint = new IPEndPoint(ipAddress, 11000);
             Socket listener = new Socket(ipAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
             try
             {
                 listener.Bind(localEndPoint);
                 listener.Listen(10);
-                Console.WriteLine("等待连接...");
+                Console.WriteLine($"等待连接... 本机IP: {ipAddress}");
                 while (true)
                 {
                     allDone.Reset();
@@ -90,77 +146,61 @@ namespace VideoRecvier
             Socket listener = (Socket)ar.AsyncState;
             Socket handler = listener.EndAccept(ar);
 
-            StateObject state = new StateObject();
-            state.workSocket = handler;
+            StateObject state = new StateObject
+            {
+                workSocket = handler,
+            };
 
-            // 接收文件大小
-            handler.BeginReceive(state.buffer, 0, sizeof(long), 0, new AsyncCallback(ReadFileSizeCallback), state);
+            handler.BeginReceive(state.buffer, 0, StateObject.BufferSize, 0, new AsyncCallback(ReadCallback), state);
         }
 
-        public static void ReadFileSizeCallback(IAsyncResult ar)
+        public static void ReadCallback(IAsyncResult ar)
         {
             StateObject state = (StateObject)ar.AsyncState;
             Socket handler = state.workSocket;
-            int bytesRead = handler.EndReceive(ar);
 
-            if (bytesRead == sizeof(long))
+            try
             {
-                long fileSize = BitConverter.ToInt64(state.buffer, 0);
-                state.fileSize = fileSize;
-                state.receivedSize = 0;
-                state.fileName = $"{DateTime.Now:yyyyMMddHHmmss}.{rocerd.MediaType}";
-
-                // 开始接收文件数据
-                handler.BeginReceive(state.buffer, 0, StateObject.BufferSize, 0, new AsyncCallback(ReadFileDataCallback), state);
-            }
-        }
-
-        public static void ReadFileDataCallback(IAsyncResult ar)
-        {
-            StateObject state = (StateObject)ar.AsyncState;
-            Socket handler = state.workSocket;
-            int bytesRead = handler.EndReceive(ar);
-
-            if (bytesRead > 0)
-            {
-                // 将接收到的数据直接写入文件
-                using (FileStream fs = new FileStream(state.fileName, FileMode.Append, FileAccess.Write))
+                int bytesRead = handler.EndReceive(ar);
+                if (bytesRead > 0)
                 {
-                    fs.Write(state.buffer, 0, bytesRead);
-                }
-
-                state.receivedSize += bytesRead;
-
-                if (state.receivedSize < state.fileSize)
-                {
-                    // 继续异步接收数据
-                    handler.BeginReceive(state.buffer, 0, StateObject.BufferSize, 0, new AsyncCallback(ReadFileDataCallback), state);
-                }
-                else
-                {
-                    // 完成接收，处理文件队列
-                    if (fileQueue.Count >= maxQueueSize)
+                    if (fileLenth <= 0)
                     {
-                        Console.WriteLine("队列已满，丢弃文件：{0}", state.fileName);
+                        if (docker.Count > 0)
+                        {
+                            imageQueue.Enqueue(docker);
+                            docker.Clear();
+                        }
+                        fileLenth = BitConverter.ToInt32(state.buffer, 0);
                     }
                     else
                     {
-                        fileQueue.Enqueue(state.fileName);
-                        Console.WriteLine("文件接收并加入队列：{0}", state.fileName);
+                        var result = new ArraySegment<Byte>(state.buffer, 0, fileLenth> state.buffer.Length? state.buffer.Length: state.buffer.Length- fileLenth).ToArray();
+                        docker.AddRange(result);
+                        fileLenth -= result.Length;
                     }
-                    handler.Close();
+                   
+
+                    handler.BeginReceive(state.buffer, 0, StateObject.BufferSize, 0, new AsyncCallback(ReadCallback), state);
                 }
+              
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"数据接收错误: {e.Message}");
             }
         }
+
 
         public class StateObject
         {
             public Socket workSocket = null;
-            public const int BufferSize = 1024;
+            public const int BufferSize = 1024*10;
             public byte[] buffer = new byte[BufferSize];
-            public string fileName;
-            public long fileSize;
-            public long receivedSize;
         }
+
+
+
+
     }
 }
